da163d32d9cb41299721358c874350f425001931
TanakaMana <makkuromyaon@gmail.com>
2017-04-17 13:53:54 +0900

    users_controllerにeditアクションを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_user, only: [:show]
+  before_action :set_user, only: [:show, :edit]
 
   def index
     @kanto_users = User.where(location: 0)
@@ -9,6 +9,9 @@ class UsersController < ApplicationController
   end
 
   def show
+  end
+
+  def edit
   end
 
   private
