6fb9a83e97749162d12567c660282dcc489cdfcc
TanakaMana <makkuromyaon@gmail.com>
2017-04-20 13:34:41 +0900

    edu_nameのenumからleaderを削除
    別途役割のステータスがあるから

--- app/models/edu_team.rb
+++ app/models/edu_team.rb
@@ -1,5 +1,5 @@
 class EduTeam < ApplicationRecord
   belongs_to :user
 
-  enum team_name: { writer: 0, plan: 1, qm: 2, bc: 3, hr: 4, account: 5, system: 6, leader: 7 }
+  enum team_name: { writer: 0, plan: 1, qm: 2, bc: 3, hr: 4, account: 5, system: 6 }
 end
