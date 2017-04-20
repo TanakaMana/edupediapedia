2c7891dbd447e024e7d346ffd5efc78c963e2975
TanakaMana <makkuromyaon@gmail.com>
2017-04-20 09:58:24 +0900

    edu_teamのカラムを追加・マイグレーションを実行

--- db/migrate/20170420005604_create_edu_teams.rb
+++ db/migrate/20170420005604_create_edu_teams.rb
@@ -1,7 +1,8 @@
 class CreateEduTeams < ActiveRecord::Migration[5.0]
   def change
     create_table :edu_teams do |t|
-
+      t.integer    :team_name
+      t.references :user
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170420004811) do
+ActiveRecord::Schema.define(version: 20170420005604) do
+
+  create_table "edu_teams", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "team_name"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_edu_teams_on_user_id", using: :btree
+  end
 
   create_table "roje_projects", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "project_name"
