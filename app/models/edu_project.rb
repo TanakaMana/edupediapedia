983d0e4d295d9cf7869a7ff024586e9e1fa401cb
TanakaMana <makkuromyaon@gmail.com>
2017-04-20 09:51:40 +0900

    テーブル名変更のためedu_projectsテーブルの生成を削除

--- app/models/edu_project.rb
+++
@@ -1,2 +0,0 @@
-class EduProject < ApplicationRecord
-end
--- db/migrate/20170420004607_create_edu_projects.rb
+++
@@ -1,8 +0,0 @@
-class CreateEduProjects < ActiveRecord::Migration[5.0]
-  def change
-    create_table :edu_projects do |t|
-
-      t.timestamps
-    end
-  end
-end
