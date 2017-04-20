ee1b475c39628df86be09933fb71d015dec6a561
TanakaMana <makkuromyaon@gmail.com>
2017-04-20 09:54:55 +0900

    roje_projectsのカラムを追加・マイグレーションの実行

--- db/migrate/20170420004811_create_roje_projects.rb
+++ db/migrate/20170420004811_create_roje_projects.rb
@@ -1,7 +1,8 @@
 class CreateRojeProjects < ActiveRecord::Migration[5.0]
   def change
     create_table :roje_projects do |t|
-
+      t.integer    :project_name
+      t.references :user
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170407231355) do
+ActiveRecord::Schema.define(version: 20170420004811) do
+
+  create_table "roje_projects", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "project_name"
+    t.integer  "user_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.index ["user_id"], name: "index_roje_projects_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
