082dbaf121327e6bf348e3ade3f201a34bef632d
TanakaMana <makkuromyaon@gmail.com>
2017-06-02 00:59:15 +0900

    CSV出力用のルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'registrations' }
 
   root 'users#index'
-  resources :users, only: [:index, :show, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update] do
+    collection do
+      get :csv_output
+    end
+  end
   resources :projects
 end
