e5c0d02cfdd17c8f0126972f41174534cff3b676
TanakaMana <makkuromyaon@gmail.com>
2017-06-24 18:55:12 +0900

    usersのroutingにdestroyを追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'registrations' }
 
   root 'users#index'
-  resources :users, only: [:index, :show, :edit, :update] do
+  resources :users, only: [:index, :show, :edit, :update, :destroy] do
     collection do
       get :csv_output
     end
